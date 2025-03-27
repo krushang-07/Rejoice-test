c0f9c03f0dbc54ffee5f37bd1067f2ff5af47d98
krRejoice <krushang314.rejoice@gmail.com>
2025-03-27 13:31:12 +0530

    production bugs

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "https://rejoice-test.vercel.app/",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: [
